977d2edc6830a5b87bafb89a1dd5dbf4ecb824b1
GoG_Paulie <ppolih21@gmail.com>
2020-10-07 10:47:03 -0400

    bring rudux into Homescreen

--- front-end/src/actions/productActions.js
+++ front-end/src/actions/productActions.js
@@ -1,20 +1,27 @@
-import axios from 'axios'
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants'
+import axios from 'axios';
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from '../constants/productConstants';
 
-export const listProducts = () => async (dispatch) {
+export const listProducts = () => async (dispatch) => {
   try {
-    dispatch({ type: PRODUCT_LIST_REQUEST })
+    dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get('/api/products')
+    const { data } = await axios.get('/api/products');
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
-      payload: data
-    })
+      payload: data,
+    });
   } catch (err) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
-      payload: err.response && err.response.data.message ? err.response.data.message : err.message
-    })
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
   }
-}
+};
