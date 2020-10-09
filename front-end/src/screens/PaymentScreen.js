a33558d9371c2767d4ca386324fa47c811dcac72
GoG_Paulie <ppolih21@gmail.com>
2020-10-09 10:43:44 -0400

    adds payment method screen

---
+++ front-end/src/screens/PaymentScreen.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Button, Form, Container, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps'
+import { savePaymentMethod } from '../actions/cartActions'
+
+const PaymentScreen = ({ history }) => {
+  const cart = useSelector(state => state.cart);
+  const { shippingAddress } = cart;
+
+  if (!shippingAddress) {
+    history.push('/shipping')
+  }
+
+  const [paymentMethod, setPaymentMethod] = useState('PayPal')
+  
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod))
+    history.push('/placeorder')
+  }
+
+  return (
+    <Container>
+      <CheckoutSteps step1 step2 step3/>
+      <FormContainer>      
+        <h1>Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group>
+            <Form.Label as='legend'>Select Method</Form.Label>
+            <Col>
+              <Form.Check type='radio' label='PayPal or Credit Card' id='PayPal' name='paymentMethod' value='PayPal' checked onChange={(e) => setPaymentMethod(e.target.value)}></Form.Check>
+            </Col>
+          </Form.Group>
+          <Button type='submit' variant='primary' className='btn-block'>Continue</Button>
+        </Form>
+    </FormContainer>
+    </Container>
+    
+  )
+}
+
+export default PaymentScreen
+
