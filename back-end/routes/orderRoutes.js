1f5b7513a766634db7831480470b9d7f3c45876c
GoG_Paulie <ppolih21@gmail.com>
2020-10-09 17:21:53 -0400

    adds backend order fetch, frontend order details reducer/action

--- back-end/routes/orderRoutes.js
+++ back-end/routes/orderRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
+// Make sure routes with :id are at the bottom to avoid routes below
+// it to search from an id
+router.route('/:id').get(protect, getOrderById);
 
 export default router;
