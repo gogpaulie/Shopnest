e84cae3d84dfea99d400bd2b3189aac685d5b1cb
GoG_Paulie <ppolih21@gmail.com>
2020-10-10 11:46:41 -0400

    adds frontend orderPay action, contantants, reducer

--- front-end/src/screens/OrderScreen.js
+++ front-end/src/screens/OrderScreen.js
@@ -33,7 +33,7 @@ const OrderScreen = ({ match }) => {
     if (!order || order._id !== orderId) {
       dispatch(getOrderDetails(orderId));
     }
-  }, [order, orderId]);
+  }, [order, orderId, dispatch]);
 
   return loading ? (
     <Spinner />
